8ea9a10829bdcc33515c6e82550ee4f0fb326309
Endrole916 <1311621474@qq.com>
2025-04-07 15:43:30 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '“向这狗娘养的世界，献上我全部的爱”',
+    '“当你投出纸飞机时，你会许下什么愿望？”',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
