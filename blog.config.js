e42df7cef73cf5379146f8a5d5d813736dbfc630
Endrole916 <1311621474@qq.com>
2025-04-07 22:10:00 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '真中合欢', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '向神发誓，我赌上一生来爱你。', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '早安、你好、晚安。还有，再见。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'www.nekofile.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || 'https://i1.hdslb.com/bfs/article/45c14ca4b55994a91ab0e776ee8f704a1d11ecb9.jpg@1112w_1200h.avif', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
